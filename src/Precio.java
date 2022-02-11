f39a98563e74ead0349067ecfdda39aa35857764
0507-susi <99457806+0507-susi@users.noreply.github.com>
2022-02-11 13:32:06 -0500

    Update Precio.java

--- src/Precio.java
+++ src/Precio.java
@@ -1,5 +1,16 @@
 	
 	
 	public class Precio implements Comparator<Libros> {
+		@Override
+		public int compare(Libros l1, Libros l2) {
+			if(l1.getPrecio()>l2.getPrecio()) {
+				return -1;
+			}
+			if(l1.getPrecio()<l2.getPrecio()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 	
 }
