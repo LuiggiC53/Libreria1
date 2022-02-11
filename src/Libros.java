fa06663ed21d5d2fc00f1ae27f6da676ca46118a
0507-susi <99457806+0507-susi@users.noreply.github.com>
2022-02-11 13:33:49 -0500

    Update Libros.java

--- src/Libros.java
+++ src/Libros.java
@@ -12,6 +12,41 @@
 			this.cantidad = cantidad;
 		}
 		
-	
+	public String getIsbn() {
+			return isbn;
+		}
+		public void setIsbn(String isbn) {
+			this.isbn = isbn;
+		}
+		public String getTitulo() {
+			return titulo;
+		}
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+		public double getPrecio() {
+			return precio;
+		}
+		public void setPrecio(double precio) {
+			this.precio = precio;
+		}
+		public int getCantidad() {
+			return cantidad;
+		}
+		public void setCantidad(int cantidad) {
+			this.cantidad = cantidad;
+		}
+		
+		
+		@Override
+		public boolean equals(Object obj) {
+			Libros l = (Libros)obj;
+			return this.isbn.equals(l.isbn);
+			
+		}
+		@Override
+		public String toString() {
+			return "ISBN: "+ this.isbn + "  titulo: " + this.titulo + "Cantidad: " + this.cantidad + "Precio: " + this.precio;
+		}
 		
 	}
